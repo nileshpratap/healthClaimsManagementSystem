b344fbd9f85d472f80fc944d53b55049a66ef2dd
nileshpratap <nileshpratap03@gmail.com>
2024-02-08 11:42:01 +0530

    node server running on port 5000

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
+});
+
+server.listen(5000, () => {
+  console.log("Server listening at port 5000");
+});
