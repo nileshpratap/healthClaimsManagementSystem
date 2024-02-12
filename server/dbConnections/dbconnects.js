49543613d168217dd873cf4f4a8b104ffe5f1d30
nileshpratap <nileshpratap03@gmail.com>
2024-02-12 15:27:35 +0530

    postgres: Server-DB connection implemented

--- server/dbConnections/dbconnects.js
+++ server/dbConnections/dbconnects.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
+import pg from "pg";
+const { Client } = pg;
 import dotenv from "dotenv";
 
 // config dotenv
 dotenv.config();
+
+// creating a client to connect with postgres
+const dbname = "HCMS DB";
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5500,
+  password: "nvp65903",
+  database: dbname,
+});
 
 mongoose.set("strictQuery", true);
 const url = process.env.MONGO_URL;
@@ -16,12 +28,24 @@ const connectMongoDB = async () => {
     process.exit(1);
   }
 };
-const connectPostgres = async () => {};
+
+const connectPostgres = async () => {
+  try {
+    await client.connect();
+    console.log(`Connected  to Postgres DB named: ${dbname}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 const connectDBs = async () => {
-  console.log("connecting DBs");
-  connectMongoDB();
+  console.log("establishing Server-DB connection");
+  // connectMongoDB();
   connectPostgres();
 };
 
+// Export the 'client' instance as a named export
+export { client };
+// Export the 'connectDBs' function as the default export
 export default connectDBs;
